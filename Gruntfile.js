0bc79b8ec5f02d85aa919acc0d00bf15dd59d12c
Shern Shiou Tan <shernshiou@gmail.com>
2013-05-03 00:44:34 +0800

    Switch to grunt-simple-mocha instead

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,20 @@
 module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'tap'
+      },
+
+      all: { src: 'test/*' }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.registerTask('test', 'simplemocha');
 };
