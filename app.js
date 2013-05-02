e1abfe264d2c768cd4254b0855523da535309c0a
Shern Shiou Tan <shernshiou@gmail.com>
2013-05-02 17:18:15 +0800

    Add jade

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var express = require('express');
+
+var app = module.exports = express();
+
+// Configuration
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(db.middleware);
+  app.use(app.router);
+});
+
+// Start server
+app.listen(3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
