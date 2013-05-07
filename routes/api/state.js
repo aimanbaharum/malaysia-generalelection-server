321926bb9b1f2a874d9af1239ec6107972ac2d19
Shern Shiou Tan <shernshiou@gmail.com>
2013-05-07 18:07:17 +0800

    Include parliament geo location at state seat

--- routes/api/state.js
+++ routes/api/state.js
@@ -1,4 +1,5 @@
 var _ = require('underscore')
+  , parliament_seats = require('../../data/parliament_seats')
   , state_seats = require('../../data/state_seats');
 
 exports.all = function (req, res) {
@@ -8,6 +9,9 @@ exports.all = function (req, res) {
 exports.findSingle = function(req, res) {
   var found = _.findWhere(state_seats, {id: req.params.id.toUpperCase()});
   if(found) {
+    var parliament = _.findWhere(parliament_seats, {id: found.parliament_id});
+    found.parliament_geo_lat = parliament.geo_lat;
+    found.parliament_geo_lng = parliament.geo_lng;
     res.json(found);
   } else {
     res.send(404);
